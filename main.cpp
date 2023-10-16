c194c9c384dd5937e0f739b7550880abbb37f2d4
Pain31415 <Cherniy412@ukr.net>
2023-10-16 20:20:58 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\t\t\t\t\tEvery hunter wants to know\n";
+    std::cout << "\t\t\t\t\tWhere the pheasant's plumage goes.\n";
+    std::cout << "\t\t\t\t\tHawks in air and beagles fleet\n";
+    std::cout << "\t\t\t\t\tWill follow after with their feet." << std::endl;
+
+    return 0;
+}
