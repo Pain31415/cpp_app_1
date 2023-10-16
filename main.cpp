528c992d12081e9c73a2e15a0e4fb3d6e33a32bb
Pain31415 <Cherniy412@ukr.net>
2023-10-16 20:20:58 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+
+	std::cout << "Продається ноутбук" << std::endl;
+	std::cout << "Модель: Lenovo ThinkPad X1 Carbon." << std::endl;
+	std::cout << "Рік випуску: 2021." << std::endl;
+	std::cout << "Ціна: 30000 грн." << std::endl;
+	std::cout << "Кількість: 1." << std::endl;
+	std::cout << "Виробник: Китай." << std::endl;
+	std::cout << "Колір: Чорний." << std::endl;
+	std::cout << "Вага: 1.09 кг." << std::endl;
+	std::cout << "Діагональ: 14 дюймів." << std::endl;
+	std::cout << "Операційна система: Windows 10 Pro." << std::endl;
+	std::cout << "Процесор: Intel Core i7-1165G7." << std::endl;
+	std::cout << "Оперативна пам'ять: 16 ГБ." << std::endl;
+	std::cout << "Відеокарта: Intel Iris Xe Graphics." << std::endl;
+	std::cout << "Тип пам'яті: LPDDR4X." << std::endl;
+	std::cout << "Об'єм SSD: 512 ГБ." << std::endl;
+	std::cout << "Тип SSD: M.2 2280 PCIe NVMe." << std::endl;
+	std::cout << "Ємність акумулятора: 57 Вт*год." << std::endl;
+	std::cout << "Тип акумулятора: Li-Pol." << std::endl;
+	std::cout << "Розміри: 315.8 x 221.6 x 14.9 мм." << std::endl;
+	std::cout << "Комплектація: ноутбук, зарядний пристрій, документація." << std::endl;
+	std::cout << "Гарантія: 12 місяців." << std::endl;
+	std::cout << "Контактний номер: +380 99 999 99 99." << std::endl;
+
+	
+	std::cout <<"\nТабличний вигляд:" << std::endl;
+	std::cout <<"+380" << std::endl;
+	std::cout <<"99 | 999" << std::endl;
+	std::cout <<"99 | 99 | 99" << std::endl;
+
+	return 0;
+}
