7eebcb1271b6b2dfb18a4f89b79095caef3d69f0
Pain31415 <Cherniy412@ukr.net>
2023-10-16 20:20:58 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "In the forest, dark and deep, \n"
+        << "Where secrets in the shadows sleep,\n"
+        << "Whispers of the ancient trees,\n"
+        << "Bring to life the mysteries." << std::endl;
+	return 0;
+}
